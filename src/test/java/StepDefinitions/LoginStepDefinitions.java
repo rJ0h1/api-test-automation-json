ed8ac4d8da8b292678dce0902e57bdd1f59591c3
rJ0h1 <rabia.joachimsmann@sogeti.com>
2025-09-22 12:19:44 +0200

    Clean start Exercise-3

---
+++ src/test/java/StepDefinitions/LoginStepDefinitions.java
@@ -0,0 +1,14 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.restassured.RestAssured;
+
+public class LoginStepDefinitions {
+
+    @Given("user wants to access the wizard api")
+    public void userWantsToAccessTheWizardWorldApi() {
+
+        RestAssured.baseURI = "https://wizard-world-api.herokuapp.com";
+
+    }
+}
